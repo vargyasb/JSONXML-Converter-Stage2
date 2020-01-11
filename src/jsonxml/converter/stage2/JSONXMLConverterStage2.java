700fe1f386dfe463530b5f4bd05dc3f278f925c4
Balazs <vargyas.b@gmail.com>
2020-01-11 17:16:03 +0100

    Stage 2 implementation

---
+++ src/jsonxml/converter/stage2/JSONXMLConverterStage2.java
@@ -0,0 +1,9 @@
+package jsonxml.converter.stage2;
+
+public class JSONXMLConverterStage2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
